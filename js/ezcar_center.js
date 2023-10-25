8bfb8c67b9c3e2c8aeaa8e7f8029494630ae2980
Guilherme <guilherme.g.andrade11@gmail.com>
2023-10-25 18:37:38 -0300

    Atualizando ezcar center

--- js/ezcar_center.js
+++ js/ezcar_center.js
@@ -33,19 +33,6 @@
     const botaoOcultoSpan = document.querySelectorAll('.botao-oculto span');
     const [span4, span5] = botaoOcultoSpan;
 
-    const formComentarios = document.querySelector('.container-post form');
-    const formComentariosTextarea = document.querySelector('.container-post form textarea');
-    const formComentariosSmall = document.querySelector('.container-post form small');
-
-    formComentarios.addEventListener('submit', e => {
-
-        if(!formComentariosTextarea.value){
-            e.preventDefault();
-            formComentariosSmall.innerHTML = 'O campo acima não pode ficar em branco!';
-        }
-    
-    });
-
     document.addEventListener('click', e => {
 
         const el = e.target;
